088d954f630d6f80f21e1018fb4b526855577d5e
pinglamb <pinglambs@gmail.com>
2013-07-25 18:15:22 +0800

    Support custom to, cc and bcc addresses

--- SendEmailService.asmx.cs
+++ SendEmailService.asmx.cs
@@ -12,7 +12,7 @@ namespace EmailApi
     /// <summary>
     /// Summary description for SendEmailService
     /// </summary>
-    [WebService(Namespace = "http://localhost/")]
+    [WebService(Namespace = "http://www.w3.org/2001/XMLSchema")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
@@ -26,14 +26,39 @@ namespace EmailApi
         private string SMTPEnableSSL = ConfigurationManager.AppSettings["SMTPEnableSSL"];
  
         [WebMethod]
-        public string SendEmail(string name)
+        public string SendEmail(string from, string to, string cc, string bcc, string subject, string body)
         {
             using (MailMessage message = new MailMessage())
             {
-                message.From = new MailAddress("info@app-zolute.com");
-                message.To.Add(new MailAddress("angusluk@app-zolute.com"));
-                message.Subject = "Test Email";
-                message.Body = "Name: " + name;
+                message.From = new MailAddress(from);
+
+                if (to.Length > 0)
+                {
+                    string[] toEmails = to.Split(',');
+                    for (int i = 0; i < toEmails.Length; i++)
+                    {
+                        message.To.Add(new MailAddress(toEmails[i]));
+                    }
+                }
+                if (cc.Length > 0)
+                {
+                    string[] ccEmails = cc.Split(',');
+                    for (int i = 0; i < ccEmails.Length; i++)
+                    {
+                        message.To.Add(new MailAddress(ccEmails[i]));
+                    }
+                }
+                if (bcc.Length > 0)
+                {
+                    string[] bccEmails = bcc.Split(',');
+                    for (int i = 0; i < bccEmails.Length; i++)
+                    {
+                        message.To.Add(new MailAddress(bccEmails[i]));
+                    }
+                }
+
+                message.Subject = subject;
+                message.Body = body;
                 SmtpClient client = new SmtpClient(SMTPServer, Convert.ToInt16(SMTPPort))
                 {
                     Credentials = new NetworkCredential(SMTPUsername, SMTPPassword),
