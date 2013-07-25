cf1cde561be89e2dc34ec473f9686f26aef4ead4
cyberportdev <developer@app-zolute.com>
2013-07-25 16:18:44 +0800

    Added Web Service Project

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ email-asp
 =========
 
 ASP Web Service For Sending Email
+
+Setup
+=========
+
+1. Enter SMTP Configuration at Web.config
+
+2. Enjoy!
+
+How to Use
+=========
+
+Just issue a normal AJAX request.
+
+    jQuery.ajax({
+      url: "/EmailApi/SendEmailService.asmx/SendEmail",
+      type: "POST",
+      data: {
+        to: "test@example.com",
+	subject: "Test Email",
+	body: "It is an email"
+      }
+    });
+
+Contributors
+=========
+
+pinglamb (from AppZolute)
+
---
+++ SendEmailService.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+
+namespace EmailApi
+{
+    /// <summary>
+    /// Summary description for SendEmailService
+    /// </summary>
+    [WebService(Namespace = "http://localhost/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class SendEmailService : System.Web.Services.WebService
+    {
+        private string SMTPServer = ConfigurationManager.AppSettings["SMTPServer"];
+        private string SMTPPort = ConfigurationManager.AppSettings["SMTPPort"];
+        private string SMTPUsername = ConfigurationManager.AppSettings["SMTPUsername"];
+        private string SMTPPassword = ConfigurationManager.AppSettings["SMTPPassword"];
+        private string SMTPEnableSSL = ConfigurationManager.AppSettings["SMTPEnableSSL"];
+ 
+        [WebMethod]
+        public string SendEmail(string name)
+        {
+            using (MailMessage message = new MailMessage())
+            {
+                message.From = new MailAddress("info@app-zolute.com");
+                message.To.Add(new MailAddress("angusluk@app-zolute.com"));
+                message.Subject = "Test Email";
+                message.Body = "Name: " + name;
+                SmtpClient client = new SmtpClient(SMTPServer, Convert.ToInt16(SMTPPort))
+                {
+                    Credentials = new NetworkCredential(SMTPUsername, SMTPPassword),
+                    EnableSsl = SMTPEnableSSL.Equals("yes")
+                };
+                client.Send(message);
+            }
+            return "success";
+        }
+    }
+}
